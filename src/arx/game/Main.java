699fcd8b78a647743fa4fe7487dc7a6bf2156fb2
Andrey <arxipmen@yandex.ru>
2015-07-08 16:09:14 +0300

    Create JFrame

---
+++ src/arx/game/Main.java
@@ -0,0 +1,13 @@
+package arx.game;
+
+import javax.swing.JFrame;
+
+public class Main {
+ public static void main(String[] args) {
+	 
+	 JFrame f = new  JFrame("Speed Car");
+	 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 f.setSize(1000, 600);
+	 f.setVisible(true);
+ }
+}
