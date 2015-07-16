5f29cde03fb2ef0240dbbb39e3f6771eca76a29c
Andrey <arxipmen@yandex.ru>
2015-07-16 15:14:41 +0300

    Did the speed limit, and fixed movement ago. Made the space on the sides.

--- src/arx/game/object/Player.java
+++ src/arx/game/object/Player.java
@@ -8,6 +8,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Player {
+	public static final int MAX_SPEED	= 100;
+	public static final int MARGIN_TOP	= 100;
+	public static final int MARGIN_DOWN	= 450;
 	
 	int speed			= 0;
 	int acceleration	= 0;
@@ -25,8 +28,11 @@ public class Player {
 	public void move() {
 		distance	+= speed;
 		speed 		+= acceleration;
+		if(speed <= 0) speed = 0;
+		if(speed >= MAX_SPEED) speed = MAX_SPEED;
 		y			-= changeY;
-		
+		if(y <= MARGIN_TOP) y = MARGIN_TOP;
+		if(y >= MARGIN_DOWN) y = MARGIN_DOWN;
 		// Create loop road
 		if(layerTwo - speed <= 0) {
 			layerOne = 0;
@@ -40,10 +46,10 @@ public class Player {
 	public void keyPressed(KeyEvent e){
 		int key = e.getKeyCode();
 		if(key == KeyEvent.VK_RIGHT) {
-			acceleration = 1;
+			acceleration = 3;
 		}
 		if(key == KeyEvent.VK_LEFT) {
-			acceleration = -1;
+			acceleration = -3;
 		}
 		if(key == KeyEvent.VK_UP) {
 			changeY = 5;
